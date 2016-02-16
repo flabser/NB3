50ebd22a9c32f17ba4312c59a5ff530db8672b64
k-zon_000 <k-zon_000@KayraNote>
2016-02-16 09:15:42 +0200

    a

--- src/kz/nextbase/script/_Validation.java
+++ src/kz/nextbase/script/_Validation.java
@@ -1,52 +1,57 @@
 package kz.nextbase.script;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 
+ * 
+ * @author Medet created 15-02-2016
+ */
 
 public class _Validation {
 
-    private List<Error> errors = new ArrayList<>();
+	private List<Error> errors = new ArrayList<>();
 
-    public void addError(String field, String error, String msg) {
-        errors.add(new Error(field, error, msg));
-    }
+	public void addError(String field, String error, String msg) {
+		errors.add(new Error(field, error, msg));
+	}
 
-    public List<Error> getErrors() {
-        return errors;
-    }
+	public List<Error> getErrors() {
+		return errors;
+	}
 
-    public boolean hasError() {
-        return errors.size() > 0;
-    }
+	public boolean hasError() {
+		return errors.size() > 0;
+	}
 
-    public class Error {
+	public class Error {
 
-        private String field;
-        private String error;
-        private String message;
+		private String field;
+		private String error;
+		private String message;
 
-        public Error(String field, String error, String message) {
-            this.field = field;
-            this.error = error;
-            this.message = message;
-        }
+		public Error(String field, String error, String message) {
+			this.field = field;
+			this.error = error;
+			this.message = message;
+		}
 
-        public String getField() {
-            return field;
-        }
+		public String getField() {
+			return field;
+		}
 
-        public String getError() {
-            return error;
-        }
+		public String getError() {
+			return error;
+		}
 
-        public String getMessage() {
-            return message;
-        }
+		public String getMessage() {
+			return message;
+		}
 
-        public String toString() {
-            return field + ", " + error + ", " + message;
-        }
-    }
+		@Override
+		public String toString() {
+			return field + ", " + error + ", " + message;
+		}
+	}
 }
