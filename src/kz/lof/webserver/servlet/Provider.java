effdde7a9e4caa51b7e2f402c55af7a3f32d41a6
k-zon_000 <k-zon_000@KayraNote>
2016-03-01 13:21:48 +0200

    dx

--- src/kz/lof/webserver/servlet/Provider.java
+++ src/kz/lof/webserver/servlet/Provider.java
@@ -106,7 +106,7 @@ public class Provider extends HttpServlet {
 			response.setStatus(result.getHttpStatus());
 			if (response.getStatus() == HttpStatus.SC_INTERNAL_SERVER_ERROR) {
 				// TODO need to Improvement
-
+				result.publishAs = PublishAsType.XML;
 				ApplicationException e = new ApplicationException(context.getServletContextName(), result.getValue());
 				throw e;
 			}
