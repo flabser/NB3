8b045ce6ec7048a9f3ef4fb9bd48fa019db18653
medin84 <kalihanovm@gmail.com>
2016-03-10 18:35:37 +0600

    json response

--- src/kz/lof/scripting/_POJOListWrapper.java
+++ src/kz/lof/scripting/_POJOListWrapper.java
@@ -1,143 +1,164 @@
 package kz.lof.scripting;
 
+import kz.lof.webserver.servlet.IOutcomeObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import kz.lof.webserver.servlet.IOutcomeObject;
 
 public class _POJOListWrapper<T extends IPOJOObject> implements IOutcomeObject {
-	private String entityType = "undefined";
-	private int maxPage;
-	private long count;
-	private int currentPage;
-	private List<T> list;
-	private List<Object> jsList;
-	private String keyWord = "";
-	private _Session ses;
+    private String entityType = "undefined";
+    private int maxPage;
+    private long count;
+    private int currentPage;
+    private List<T> list;
+    private String keyWord = "";
+    private _Session ses;
 
-	public _POJOListWrapper(List<T> list, int maxPage, long count, int currentPage, _Session ses) {
-		this.maxPage = maxPage;
-		this.count = count;
-		this.currentPage = currentPage;
-		this.list = list;
-		this.ses = ses;
-		recognizeName();
-	}
+    public _POJOListWrapper(List<T> list, int maxPage, long count, int currentPage, _Session ses) {
+        this.maxPage = maxPage;
+        this.count = count;
+        this.currentPage = currentPage;
+        this.list = list;
+        this.ses = ses;
+        recognizeName();
+    }
 
-	public _POJOListWrapper(List<T> list, int maxPage, long count, int currentPage, _Session ses, String keyWord) {
-		this.maxPage = maxPage;
-		this.count = count;
-		this.currentPage = currentPage;
-		this.list = list;
-		this.ses = ses;
-		this.keyWord = " keyword=\"" + keyWord + "\" ";
-		recognizeName();
-	}
+    public _POJOListWrapper(List<T> list, int maxPage, long count, int currentPage, _Session ses, String keyWord) {
+        this.maxPage = maxPage;
+        this.count = count;
+        this.currentPage = currentPage;
+        this.list = list;
+        this.ses = ses;
+        this.keyWord = " keyword=\"" + keyWord + "\" ";
+        recognizeName();
+    }
 
-	public _POJOListWrapper(List<T> list, _Session ses) {
-		this.count = list.size();
-		this.list = list;
-		maxPage = 1;
-		currentPage = 1;
-		this.ses = ses;
-		recognizeName();
-	}
+    public _POJOListWrapper(List<T> list, _Session ses) {
+        this.count = list.size();
+        this.list = list;
+        maxPage = 1;
+        currentPage = 1;
+        this.ses = ses;
+        recognizeName();
+    }
 
-	public _POJOListWrapper(List<T> list, _Session ses, String en) {
-		this.count = list.size();
-		this.list = list;
-		maxPage = 1;
-		currentPage = 1;
-		this.ses = ses;
-		entityType = en;
-	}
+    public _POJOListWrapper(List<T> list, _Session ses, String en) {
+        this.count = list.size();
+        this.list = list;
+        maxPage = 1;
+        currentPage = 1;
+        this.ses = ses;
+        entityType = en;
+    }
 
-	public _POJOListWrapper(String msg, String keyWord) {
-		this.count = 0;
-		List<T> l = new ArrayList<T>();
-		l.add((T) new SimplePOJO(msg));
-		this.list = l;
-		this.keyWord = " keyword=\"" + keyWord + "\" ";
-	}
+    public _POJOListWrapper(String msg, String keyWord) {
+        this.count = 0;
+        List<T> l = new ArrayList<T>();
+        l.add((T) new SimplePOJO(msg));
+        this.list = l;
+        this.keyWord = " keyword=\"" + keyWord + "\" ";
+    }
 
-	public int getMaxPage() {
-		return maxPage;
-	}
+    public int getMaxPage() {
+        return maxPage;
+    }
 
-	public List<T> getList() {
-		return list;
-	}
+    public List<T> getList() {
+        return list;
+    }
 
-	@Override
-	public String toXML() {
+    private void recognizeName() {
+        try {
+            if (list.size() > 0) {
+                final Class<T> listClass = (Class<T>) list.get(0).getClass();
+                entityType = listClass.getSimpleName().toLowerCase();
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
 
-		String result = "<query entity=\"" + entityType + "\"  maxpage=\"" + maxPage + "\" count=\"" + count + "\" currentpage=\"" + currentPage
-		        + "\"" + keyWord + ">";
-		for (T val : list) {
-			result += "<entry isread=\"1\" hasattach=\"0\" id=\"" + val.getIdentifier() + "\" " + "url=\"" + val.getURL() + "\"><viewcontent>";
-			result += val.getShortXMLChunk(ses) + "</viewcontent></entry>";
-		}
-		return result + "</query>";
-	}
+        }
+    }
 
-	@Override
-	public Object toJSON() {
-		jsList = new ArrayList<Object>();
-		for (T obj : list) {
-			jsList.add(obj.getJSONObj(ses));
-		}
-		return jsList;
-	}
+    @Override
+    public String toXML() {
 
-	private void recognizeName() {
-		try {
-			if (list.size() > 0) {
-				final Class<T> listClass = (Class<T>) list.get(0).getClass();
-				entityType = listClass.getSimpleName().toLowerCase();
-			}
-		} catch (ArrayIndexOutOfBoundsException e) {
+        String result = "<query entity=\"" + entityType + "\"  maxpage=\"" + maxPage + "\" count=\"" + count + "\" currentpage=\"" + currentPage
+                + "\"" + keyWord + ">";
+        for (T val : list) {
+            result += "<entry isread=\"1\" hasattach=\"0\" id=\"" + val.getIdentifier() + "\" " + "url=\"" + val.getURL() + "\"><viewcontent>";
+            result += val.getShortXMLChunk(ses) + "</viewcontent></entry>";
+        }
+        return result + "</query>";
+    }
 
-		}
-	}
+    @Override
+    public Object toJSON() {
+        _Meta meta = new _Meta();
+        meta.count = count;
+        meta.totalPages = maxPage;
+        meta.page = currentPage;
+        meta.keyWord = keyWord;
 
-	class SimplePOJO implements IPOJOObject {
-		private String msg;
+        _Response result = new _Response();
+        result.meta = meta;
+        result.type = entityType;
+        for (T obj : list) {
+            result.list.add(obj.getJSONObj(ses));
+        }
 
-		SimplePOJO(String msg) {
-			this.msg = msg;
-		}
+        return result;
+    }
 
-		@Override
-		public String getURL() {
-			return msg;
+    public static class _Meta {
+        public int totalPages;
+        public long count;
+        public int page;
+        public String keyWord = "";
+    }
 
-		}
+    public static class _Response {
+        public _Meta meta;
+        public String type;
+        public List<Object> list = new ArrayList<>();
+    }
 
-		@Override
-		public boolean isEditable() {
-			return false;
-		}
+    class SimplePOJO implements IPOJOObject {
+        private String msg;
 
-		@Override
-		public String getFullXMLChunk(_Session ses) {
-			return "<message>" + msg + "</message>";
-		}
+        SimplePOJO(String msg) {
+            this.msg = msg;
+        }
 
-		@Override
-		public String getShortXMLChunk(_Session ses) {
-			return getFullXMLChunk(ses);
-		}
+        @Override
+        public String getURL() {
+            return msg;
 
-		@Override
-		public Object getJSONObj(_Session ses) {
-			return this;
-		}
+        }
 
-		@Override
-		public String getIdentifier() {
-			return "null";
-		}
+        @Override
+        public boolean isEditable() {
+            return false;
+        }
 
-	}
+        @Override
+        public String getFullXMLChunk(_Session ses) {
+            return "<message>" + msg + "</message>";
+        }
 
+        @Override
+        public String getShortXMLChunk(_Session ses) {
+            return getFullXMLChunk(ses);
+        }
+
+        @Override
+        public Object getJSONObj(_Session ses) {
+            return this;
+        }
+
+        @Override
+        public String getIdentifier() {
+            return "null";
+        }
+
+    }
 }
