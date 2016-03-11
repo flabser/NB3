d9ad15dad51099d5a5ad7207e1d9a52d43bbc8b5
medin84 <kalihanovm@gmail.com>
2016-03-12 01:18:50 +0600

    commit

--- src/kz/lof/scripting/_POJOObjectWrapper.java
+++ src/kz/lof/scripting/_POJOObjectWrapper.java
@@ -2,23 +2,28 @@ package kz.lof.scripting;
 
 import kz.lof.webserver.servlet.IOutcomeObject;
 
+
 public class _POJOObjectWrapper implements IOutcomeObject {
-	private IPOJOObject object;
-	private _Session ses;
+    private IPOJOObject object;
+    private _Session ses;
 
-	public _POJOObjectWrapper(IPOJOObject object, _Session ses) {
-		this.object = object;
-		this.ses = ses;
-	}
+    public _POJOObjectWrapper(IPOJOObject object, _Session ses) {
+        this.object = object;
+        this.ses = ses;
+    }
 
-	@Override
-	public String toXML() {
-		return "<document entity=\"" + object.getClass().getSimpleName().toLowerCase() + "\"  docid=\"" + object.getIdentifier() + "\" editable=\""
-		        + object.isEditable() + "\"><fields>" + object.getFullXMLChunk(ses) + "</fields></document>";
-	}
+    @Override
+    public String toXML() {
+        String entity = "";
+        if (!object.getClass().getSimpleName().equals("_EnumWrapper")) {
+            entity = object.getClass().getSimpleName().toLowerCase();
+        }
+        return "<document entity=\"" + entity + "\"  docid=\"" + object.getIdentifier() + "\" editable=\""
+                + object.isEditable() + "\"><fields>" + object.getFullXMLChunk(ses) + "</fields></document>";
+    }
 
-	@Override
-	public Object toJSON() {
-		return object.getJSONObj(ses);
-	}
+    @Override
+    public Object toJSON() {
+        return object.getJSONObj(ses);
+    }
 }
